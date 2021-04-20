bee3c4f721f7577c24b52673d888f72bcf3b9785
Luiz Gonzaga <luizgonzaga15@gmail.com>
2021-04-20 13:01:16 -0300

    Initial Commit

---
+++ CursoMVC/Models/Contexto.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CursoMVC.Models
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-OFQU7U6;Database=Cursomvc;Integrated Security=True");
+        }
+
+        public DbSet<Categoria> Categorias { get; set; }
+
+    }
+}
